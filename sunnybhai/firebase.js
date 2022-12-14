2836861ea7c19362a3ef530ed681b5fe4f04a962
Shayan Affandi <shayanaffandi5785@gmail.com>
2022-12-14 12:15:20 +0500

    First deploy

---
+++ sunnybhai/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDm82SCMVarSBVD1VuviszeBJjzP0HLQjQ",
+  authDomain: "sunny-368f3.firebaseapp.com",
+  projectId: "sunny-368f3",
+  storageBucket: "sunny-368f3.appspot.com",
+  messagingSenderId: "446265185415",
+  appId: "1:446265185415:web:cf648d639c42c53b325422"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ sunnybhai/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <labal>Enter your email</labal>
+        <input type="email" />
+        <labal>Enter your password</labal>
+        <input type="password" />
+        <input type="button" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
